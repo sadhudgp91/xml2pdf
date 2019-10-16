16c815e8af8feab5e16d269dd54286e9f622d46c
Kaushik Sadhu <st158332@stud.uni-stuttgart.de>
2019-10-16 20:52:48 +0200

    inital release files

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace xml2pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.browse = new System.Windows.Forms.Button();
+            this.fncPdf = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.zStreamBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zStreamBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(31, 118);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // fncPdf
+            // 
+            this.fncPdf.Location = new System.Drawing.Point(313, 119);
+            this.fncPdf.Name = "fncPdf";
+            this.fncPdf.Size = new System.Drawing.Size(75, 22);
+            this.fncPdf.TabIndex = 1;
+            this.fncPdf.Text = "PDF";
+            this.fncPdf.UseVisualStyleBackColor = true;
+            this.fncPdf.Click += new System.EventHandler(this.FncPdf_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "xml2pdf ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitte wählen Sie eine XML-Datei";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PDF generieren";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 163);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(419, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(57, 17);
+            this.toolStripStatusLabel1.Text = "Initialized";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 11);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(419, 185);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fncPdf);
+            this.Controls.Add(this.browse);
+            this.Name = "Form1";
+            this.Text = "XML2PDF";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zStreamBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button fncPdf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.BindingSource zStreamBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iText;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using PdfSharp.Pdf;
+
+namespace xml2pdf
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse XML Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xml",
+                Filter = "txt files (*.xml)|*.xml",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel1.Text = openFileDialog1.FileName;
+                statusStrip1.BackColor = Color.Green;
+            }
+
+            ds.ReadXml(openFileDialog1.FileName);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void FncPdf_Click(object sender, EventArgs e)
+        {
+            
+            // Converting Dataset to PDF
+            Document document = new Document();
+
+            string fn = "zugferd-invoice" + "_" + DateTime.Now.ToShortDateString();
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = fn.Replace("/", "-").Replace(" ", "_");
+            sfd.Filter = "(*.pdf)|*.pdf";
+            sfd.ShowDialog();
+            string path = sfd.FileName;
+
+            if (sfd.FileName != null)
+            {
+                //Exporting to PDF.
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(path, FileMode.Create));
+                toolStripStatusLabel1.Text = "PDF File Generated Successfully in: " + path;
+            }
+
+            document.Open();
+            for (int table = 0; table < ds.Tables.Count; table++)
+            {
+                PdfPTable pdfTable = new PdfPTable(ds.Tables[table].Columns.Count);
+                //pdfTable.AddCell(new Phrase("heading1"));
+                //pdfTable.AddCell(new Phrase("heading2"));
+                //pdfTable.AddCell(new Phrase("heading3"));
+
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+                    pdfTable.AddCell(new Phrase(dataGridView1.Columns[i - 1].HeaderText));
+                }
+
+
+                for (int row = 0; row < ds.Tables[table].Rows.Count; row++)
+                {
+
+                    for (int column = 0; column < ds.Tables[table].Columns.Count; column++)
+                    {
+                        pdfTable.AddCell(new Phrase(ds.Tables[table].Rows[row][column].ToString()));
+                    }
+                }
+                document.Add(pdfTable);
+            }
+            document.Close();
+           
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            toolStripStatusLabel1.Text = "PDF File Generated Successfully in: " + path;
+            statusStrip1.BackColor = Color.Green;
+
+        }
+
+    }
+}
